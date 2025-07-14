65ae7096bf36035302aefaad3d3666551c1a6814
timecop84 <111374507+timecop84@users.noreply.github.com>
2025-07-14 20:44:34 +0300

    Remove unused and manually deleted files from project

--- src/glad.c
+++
@@ -1,1 +0,0 @@
-// Place the generated glad.c here. Please copy the contents of glad/glad.c from your generated files into this location.
--- src/vector_test.cpp
+++
@@ -1,7 +0,0 @@
-#include "vector_test.h"
-
-int main() {
-    std::vector<int> v = {1, 2, 3};
-    for (int i : v) std::cout << i << std::endl;
-    return 0;
-}
